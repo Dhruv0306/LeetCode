a3a8e339bc3aacd8314e41ec73ff0b5c0b8e854a
Dhruv0306 <141617373+Dhruv0306@users.noreply.github.com>
2026-05-01 23:19:20 +0530

    Time: 5 ms (39.82%) | Memory: 47.1 MB (80.98%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -5,16 +5,11 @@ class Solution {
             for(int j = 0; j< grid[0].length ; j++){
                 if(grid[i][j] == 1){
                     peri += 4;
+                    if(i > 0 && grid[i - 1][j] == 1) peri--;
+                    if(i < grid.length - 1 && grid[i + 1][j] == 1) peri--;
+                    if(j > 0 && grid[i][j - 1] == 1) peri--;
+                    if(j < grid[0].length - 1 && grid[i][j + 1] == 1) peri--;
                 }
-                // System.out.println("i: " + i + " j: " + j + " grid[i][j]: " + grid[i][j] + " peri: " + peri);
-                if(i > 0 && grid[i][j] == 1 && grid[i - 1][j] == 1) peri--;
-                // System.out.println("i: " + i + " j: " + j + " grid[i][j]: " + grid[i][j] + " peri: " + peri);
-                if(i < grid.length - 1 && grid[i][j] == 1 && grid[i + 1][j] == 1) peri--;
-                // System.out.println("i: " + i + " j: " + j + " grid[i][j]: " + grid[i][j] + " peri: " + peri);
-                if(j > 0 && grid[i][j] == 1 && grid[i][j - 1] == 1) peri--;
-                // System.out.println("i: " + i + " j: " + j + " grid[i][j]: " + grid[i][j] + " peri: " + peri);
-                if(j < grid[0].length - 1 && grid[i][j] == 1 && grid[i][j + 1] == 1) peri--;
-                // System.out.println("i: " + i + " j: " + j + " grid[i][j]: " + grid[i][j] + " peri: " + peri);
             }
         }
         return peri;
