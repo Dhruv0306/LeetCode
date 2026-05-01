6914c0849cf3ac5a07bdc84184f29efbffec9569
Dhruv0306 <141617373+Dhruv0306@users.noreply.github.com>
2026-05-01 23:03:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (39.45%) - LeetSync

--- 461-hamming-distance/hamming-distance.java
+++ 461-hamming-distance/hamming-distance.java
@@ -4,13 +4,13 @@ class Solution {
         while(x > 0 || y > 0){
             int bitX = (x > 0) ? x % 2 : -1;
             int bitY = (y > 0) ? y % 2 : -1;
-            System.out.println(bitX + " " + bitY + " " + x + " " + y);
+            // System.out.println(bitX + " " + bitY + " " + x + " " + y);
             if(bitX == -1 && bitY == 1) distance++;
             if(bitX == 1 && bitY == -1) distance++;
             if(bitX != -1 && bitY != -1 && bitX != bitY) distance++;
             x >>= 1;
             y >>= 1;
-            System.out.println( x + " " + y);
+            // System.out.println( x + " " + y);
 
         }
         return distance;
