c73a4a50ecf6f18bd0a0c2d7e01aec952501bce0
Dhruv0306 <141617373+Dhruv0306@users.noreply.github.com>
2026-05-04 21:27:07 +0530

    Time: 2 ms (14.84%) | Memory: 44.9 MB (44.68%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.java
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -1,29 +1,25 @@
 class Solution {
     public String reverseStr(String s, int k) {
-        int i = 0, n = s.length();
-        StringBuilder sb = new StringBuilder();
-        while(i < n){
-            if((n - i) > 2 * k){
-                for(int j = i + k - 1; j >= i; j--){
-                    sb.append(s.charAt(j));
-                }
-                for(int j = i + k; j < (i + 2 * k); j++){
-                    sb.append(s.charAt(j));
-                }
-                i += 2 * k;
-            } else if((n - i) >= k){
-                for(int j = i + k - 1; j >= i; j--){
-                    sb.append(s.charAt(j));
-                }
-                sb.append(s.substring(i + k));
-                break;
-            } else {
-                for(int j = n - 1; j >= i; j--){
-                    sb.append(s.charAt(j));
-                }
-                break;
+
+        int step = k * 2;
+        char[] arr = s.toCharArray();
+
+        for(int i = 0; i < arr.length; i += step){
+
+            int start = i;
+            int end = Math.min(i + k - 1, arr.length - 1);
+
+            while(start < end){
+
+                char temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+
+                start++;
+                end--;
             }
         }
-        return sb.toString();
+
+        return new String(arr);
     }
 }
