f3198a898bd2dba9ab12474bd2d82751f8618b03
Dhruv0306 <141617373+Dhruv0306@users.noreply.github.com>
2026-05-04 21:25:36 +0530

    Time: 4 ms (7.57%) | Memory: 45.1 MB (9.28%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.java
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -15,37 +15,15 @@ class Solution {
                 for(int j = i + k - 1; j >= i; j--){
                     sb.append(s.charAt(j));
                 }
-                // s = reverseBetween(s, i, i + k - 1);
                 sb.append(s.substring(i + k));
                 break;
             } else {
-                // s = reverseBetween(s, i, n - 1);
                 for(int j = n - 1; j >= i; j--){
                     sb.append(s.charAt(j));
                 }
-                // sb.append(s.substring(i, n));
                 break;
             }
         }
-        return sb.toString();
-    }
-
-    public static String reverseBetween(String s, int start, int end){
-        if(start < 0 || end < 0) return "-1";
-        if(start > s.length() || end > s.length()) return "-1";
-        if(start > end) return "-1";
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < start; i++){
-            sb.append(s.charAt(i));
-        }
-        for(int i = end; i >= start; i--){
-            sb.append(s.charAt(i));
-        }
-
-        for(int i = end + 1; i < s.length(); i++){
-            sb.append(s.charAt(i));
-        }
-
         return sb.toString();
     }
 }
