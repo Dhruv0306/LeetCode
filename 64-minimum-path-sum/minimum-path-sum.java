c56f022dcf020445b40ea754992647958c8331b6
Dhruv0306 <141617373+Dhruv0306@users.noreply.github.com>
2026-05-05 12:27:21 +0530

    Time: 4 ms (62.42%) | Memory: 50.3 MB (38.78%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -9,7 +9,7 @@ class Solution {
         for(int i = 1; i < m; i++){
             dp[i][0] = dp[i - 1][0] + grid[i][0];
         }
-
+    
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
                 dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
