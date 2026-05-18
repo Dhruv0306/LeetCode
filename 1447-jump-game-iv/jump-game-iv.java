2995e60181f8ab918ba1311a17bd64d7800ad15d
Dhruv0306 <141617373+Dhruv0306@users.noreply.github.com>
2026-05-18 10:58:23 +0530

    Time: 48 ms (87.48%) | Memory: 71.3 MB (96.82%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.java
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -1,58 +1,47 @@
 class Solution {
     public int minJumps(int[] arr) {
-        HashMap<Integer, HashSet<Integer>> numIndexMap = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            numIndexMap.computeIfAbsent(arr[i], k -> new HashSet<>()).add(i);
-        }
-        boolean[] visited = new boolean[arr.length];
-        boolean[] added = new boolean[arr.length];
-        int[] jumps = new int[arr.length];
-        int size = arr.length;
-        Arrays.fill(jumps, Integer.MAX_VALUE);
-        jumps[0] = 0;
-        ArrayList<Integer> jumpList = new ArrayList<>();
-        jumpList.add(0);
-        added[0] = true;
-        for (int i = 0; i < jumpList.size(); i++) {
-            int currIndex = jumpList.get(i);
-            if(visited[currIndex]) {
-                continue;
-            }
-            visited[currIndex] = true;
-            if (currIndex - 1 > 0 && !added[currIndex - 1] && !visited[currIndex - 1]) {
-                jumpList.add(currIndex - 1);
-                if(jumps[currIndex - 1] > jumps[currIndex] + 1) {
-                    jumps[currIndex - 1] = jumps[currIndex] + 1;
-                    added[currIndex - 1] = true;
-                }
-            }
-            if (currIndex + 1 < size && !added[currIndex + 1] && !visited[currIndex + 1]) {
-                jumpList.add(currIndex + 1);
-                if(jumps[currIndex + 1]> jumps[currIndex] + 1) {
-                    jumps[currIndex + 1] = jumps[currIndex] + 1;
-                    added[currIndex + 1] = true;
-                }
-                if(currIndex + 1 == size - 1) {
-                    break;
-                }
-            }
-            HashSet<Integer> numIndexSet;
-            if (numIndexMap.containsKey(arr[currIndex])) {
-                numIndexSet = numIndexMap.get(arr[currIndex]);
-                for(Integer index : numIndexSet) {
-                    if (!added[index] && !visited[index]) {
-                        jumpList.add(index);
-                        if(jumps[index] > jumps[index] + 1) {
-                            jumps[index] = jumps[currIndex] + 1;
-                        }
-                        if(index == size - 1) {
-                            break;
-                        }
+        int n = arr.length, step = 0;
+        if(n == 1) return 0;
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int i = 0; i < n; i++)
+            map.computeIfAbsent(arr[i], k->new ArrayList<>()).add(i);
+        
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[n];
+        
+        q.offer(0);
+        vis[0] = true;
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            step++;
+
+            for(int i = 0; i < size; i++) {
+                int curId = q.poll();
+
+                int[] nextPos = {curId+1, curId-1};
+                for(int nextId : nextPos) {
+                    if(nextId == n-1) return step;
+                    if(nextId >= 0 && nextId < n && !vis[nextId]) {
+                        vis[nextId] = true;
+                        q.offer(nextId); 
                     }
                 }
-                numIndexMap.remove(arr[currIndex]);
+
+                if(map.get(arr[curId]) != null) {
+                    for(int nextId : map.get(arr[curId])){
+                        if(nextId == n-1) return step;
+                        if(!vis[nextId]) {
+                            vis[nextId] = true;
+                            q.offer(nextId);
+                        }
+                    }
+                    map.remove(arr[curId]);
+                }
             }
         }
-        return jumps[size - 1];
+
+        return step;
     }
 }
