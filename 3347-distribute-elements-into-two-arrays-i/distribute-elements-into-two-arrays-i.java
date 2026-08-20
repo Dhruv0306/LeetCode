68ad9bbd4471cabd54cb602c9e21c291c259e39d
Dhruv0306 <141617373+Dhruv0306@users.noreply.github.com>
2026-08-20 13:24:18 +0530

    Time: 1 ms (98.08%) | Memory: 46.7 MB (66.35%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -3,29 +3,30 @@ class Solution {
         int len = nums.length, j = 1, k = 1;
         int[] nums1 = new int[len];
         int[] nums2 = new int[len];
-        Arrays.fill(nums1,-1);
-        Arrays.fill(nums2,-1);
+        Arrays.fill(nums1, -1);
+        Arrays.fill(nums2, -1);
         nums1[0] = nums[0];
         nums2[0] = nums[1];
-        
-        for(int i = 2; i < len; i++){
-            if(nums1[j-1] > nums2[k-1]){
+
+        for (int i = 2; i < len; i++) {
+            if (nums1[j - 1] > nums2[k - 1]) {
                 nums1[j++] = nums[i];
-            }else{
+            } else {
                 nums2[k++] = nums[i];
             }
         }
         j = 0;
-        for(int i: nums1){
-            if(i == -1) break;
+        for (int i : nums1) {
+            if (i == -1)
+                break;
             nums[j++] = i;
         }
-        for(int i: nums2){
-            if(i == -1) break;
+        for (int i : nums2) {
+            if (i == -1)
+                break;
             nums[j++] = i;
         }
-        
-        
+
         return nums;
     }
 }
